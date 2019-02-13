704ad9d838cdc339299efa41a0781c188b764dc7
Alejandro Sankan <asankan@msn.com>
2019-02-13 18:48:46 -0400

    13/02 Termino de evaluacion

--- EVFI_Calculadora/js/app.js
+++ EVFI_Calculadora/js/app.js
@@ -1,79 +1,74 @@
 //Declaramos variables
 var operacion='';
-var tecla='';
 var resultado = 0;
 var operador  = 0;
 var entero = 10;
 var decimal = 1;
 
+
 // eventos de mouse o teclado.
 function presPunto( ){
-  tecla = '.';
   document.getElementById('punto').style = "padding:5px; background-color: #999999";
-  operador = operador+'.';
-  document.querySelector("#calculadoraFondo #display").innerHTML = operador
-  decimal = 0.1;
-  entero = 1;
+
+  if ( operador.toString().length < 8) {
+    if (decimal == 1) {
+      operador = operador+'.';
+      decimal = 0.1;
+      entero = 1;
+    }
+    document.querySelector("#calculadoraFondo #display").innerHTML = operador;
+  }
 }
+
+function ingresaDigito( tecla ) {
+  if (operador.toString().length < 8) {
+    operador = operador * entero + decimal * tecla;
+    if (decimal != 1) {
+      decimal = decimal / 10;
+    }
+  }
+  document.querySelector("#calculadoraFondo #display").innerHTML = operador;
+}
+
 function pres1( ){
   document.getElementById('1').style = "padding:5px; background-color: #999999";
-  operador = operador*entero + decimal*2;
-  document.querySelector("#calculadoraFondo #display").innerHTML = operador;
-  if (decimal != 1) {decimal = decimal / 10;}
+  ingresaDigito(1);
 }
 function pres2 ( ){
   document.getElementById( '2' ).style = "padding:5px; background-color: #999999";
-  operador = operador*entero + decimal*2;
-  document.querySelector("#calculadoraFondo #display").innerHTML = operador;
-  if (decimal != 1) {decimal = decimal / 10;}
+  ingresaDigito(2);
 }
 function pres3 ( ){
   document.getElementById('3').style = "padding:5px; background-color: #999999";
-  operador = operador*entero + decimal*3;
-  document.querySelector("#calculadoraFondo #display").innerHTML = operador;
-  if (decimal != 1) {decimal = decimal / 10;}
+  ingresaDigito(3);
 }
 function pres4 ( ){
   document.getElementById('4').style = "padding:5px; background-color: #999999";
-  operador = operador*entero + decimal*4;
-  document.querySelector("#calculadoraFondo #display").innerHTML = operador;
-  if (decimal != 1) {decimal = decimal / 10;}
+  ingresaDigito(4);
 }
 function pres5( ){
   document.getElementById('5').style = "padding:5px; background-color: #999999";
-  operador = operador*entero + decimal*5;
-  document.querySelector("#calculadoraFondo #display").innerHTML = operador;
-  if (decimal != 1) {decimal = decimal / 10;}
+  ingresaDigito(5);
 }
 function pres6( ){
   document.getElementById('6').style = "padding:5px; background-color: #999999";
-  operador = operador*entero + decimal*6;
-  document.querySelector("#calculadoraFondo #display").innerHTML = operador;
-  if (decimal != 1) {decimal = decimal / 10;}
+  ingresaDigito(6);
 }
 function pres7 ( ){
   document.getElementById('7').style = "padding:5px; background-color: #999999";
-  operador = operador*entero + decimal*7;
-  document.querySelector("#calculadoraFondo #display").innerHTML = operador;
-  if (decimal != 1) {decimal = decimal / 10;}
+  ingresaDigito(7);
 }
 function pres8( ){
   document.getElementById('8').style = "padding:5px; background-color: #999999";
-  operador = operador*entero + decimal*8;
-  document.querySelector("#calculadoraFondo #display").innerHTML = operador;
-  if (decimal != 1) {decimal = decimal / 10;}
+  ingresaDigito(8);
 }
 function pres9( ){
   document.getElementById('9').style = "padding:5px; background-color: #999999";
-  operador = operador*entero + decimal*9;
-  document.querySelector("#calculadoraFondo #display").innerHTML = operador;
-  if (decimal != 1) {decimal = decimal / 10;}
+  ingresaDigito(9);
 }
 function pres0( ){
   document.getElementById('0').style = "padding:5px; background-color: #999999";
-  operador = operador*entero + decimal*0;
-  document.querySelector("#calculadoraFondo #display").innerHTML = operador;
-  if (decimal != 1) {decimal = decimal / 10;}
+  ingresaDigito(0);
 }
 function presSign( ){
   document.getElementById('sign').style = "padding:5px; background-color: #999999";
@@ -93,29 +88,61 @@ function limpiar() { // cuando se inytroduce una operacion
   operador = 0;
   entero = 10;
   decimal = 1;
+  document.querySelector("#calculadoraFondo #display").innerHTML = operador;
+}
+
+function calculaResultado(){
+  if (operacion != '') {
+    resultado = eval( resultado + operacion + operador );
+    if ( resultado.toString().length > 8) {
+      resultado = resultado.toString().substr(1,8);
+    }
+    document.querySelector("#calculadoraFondo #display").innerHTML = resultado
+  }
+  else {
+      resultado = operador;
+  }
+  operador = 0;
+  entero = 10;
+  decimal = 1;
 }
 
 function presMas(){
-
+  document.getElementById('mas' ).style = "padding:5px; background-color: #999999";
+  calculaResultado();
+  operacion = '+';
 }
+
 function presMenos(){
-
+  document.getElementById('menos' ).style = "padding:5px; background-color: #999999";
+  calculaResultado();
+  operacion = '-';
 }
+
 function presPor(){
-
+  document.getElementById('por' ).style = "padding:5px; background-color: #999999";
+  calculaResultado();
+  operacion = '*';
 }
+
 function presDiv(){
-
+  document.getElementById('dividido' ).style = "padding:5px; background-color: #999999";
+  calculaResultado();
+  operacion = '/';
 }
+
 function presRaiz(){
-
 }
+
 function presIgual(){
-
+  document.getElementById('igual').style = "padding:5px; background-color: #999999";
+  calculaResultado();
+  operacion = '';
+  operador = resultado;
 }
+
 // fin de presion de teclas
 
-function depresPunto (){ document.getElementById('punto').style = "padding: 0";}
 function depres1 (){ document.getElementById('1').style = "padding: 0";}
 function depres2 (){ document.getElementById('2').style = "padding: 0";}
 function depres3 (){ document.getElementById('3').style = "padding: 0";}
@@ -126,6 +153,7 @@ function depres7 (){ document.getElementById('7').style = "padding: 0";}
 function depres8 (){ document.getElementById('8').style = "padding: 0";}
 function depres9 (){ document.getElementById('9').style = "padding: 0";}
 function depres0 (){ document.getElementById('0').style = "padding: 0";}
+function depresPunto (){ document.getElementById('punto').style = "padding: 0";}
 function depresSign (){ document.getElementById('sign').style = "padding: 0";}
 function depresReset (){ document.getElementById('on').style = "padding: 0";}
 function depresMas (){ document.getElementById('mas').style = "padding: 0";}
@@ -135,7 +163,59 @@ function depresPor (){ document.getElementById('por').style = "padding: 0";}
 function depresRaiz (){ document.getElementById('raiz').style = "padding: 0";}
 function depresIgual (){ document.getElementById('igual').style = "padding: 0";}
 
-// otros
+function presionaTecla(event) {
+  var tecla=event.wich||event.keyCode;
+  switch ( tecla ) {
+    case 48: pres0(); depres0(); break;
+    case 49: pres1(); depres1(); break;
+    case 50: pres2(); depres2(); break;
+    case 51: pres3(); depres3(); break;
+    case 52: pres4(); depres4(); break;
+    case 53: pres5(); depres5(); break;
+    case 54: pres6(); depres6(); break;
+    case 55: pres7(); depres7(); break;
+    case 56: pres8(); depres8(); break;
+    case 57: pres9(); depres9(); break;
+    case 43: presMas(); depresMas(); break;
+    case 45: presMenos(); depresMenos(); break;
+    case 42: presPor(); depresPor(); break;
+    case 47: presDiv(); depresDiv(); break;
+    case 13: presIgual(); depresIgual(); break;
+    case 61: presIgual(); depresIgual(); break;
+    case 46: presPunto(); depresPunto(); break;
+    case 127: presReset(); depresReset(); break;
+    case 08: presReset(); depresReset(); break;
+
+  }
+}
+
+function presionaTecla2(event){
+  tecla=event.wich||event.keyCode;
+  switch ( String.fromCharCode(tecla) ) {
+    case '0': pres0(); depres0(); break;
+    case '1': pres1(); depres1(); break;
+    case '2': pres2(); depres2(); break;
+    case '3': pres3(); depres3(); break;
+    case '4': pres4(); depres4(); break;
+    case '5': pres5(); depres5(); break;
+    case '6': pres6(); depres6(); break;
+    case '7': pres7(); depres7(); break;
+    case '8': pres8(); depres8(); break;
+    case '9': pres9(); depres9(); break;
+    case '+': presMas(); depresMas(); break;
+    case '-': presMenos(); depresMenos(); break;
+    case '*': presPor(); depresPor(); break;
+    case '/': presDiv(); depresDiv(); break;
+    case '=': presIgual(); depresIgual(); break;
+    case '.': presPunto(); depresPunto(); break;
+    case '': presReset(); depresReset(); break;
+
+  }
+}
+
+
+
+// presion de mouse...
 
 document.getElementById('punto').onmousedown = presPunto;
 document.getElementById('1').onmousedown = pres1;
@@ -176,3 +256,6 @@ document.getElementById('dividido').onmouseup = depresDiv;
 document.getElementById('por').onmouseup = depresPor;
 document.getElementById('raiz').onmouseup = depresRaiz;
 document.getElementById('igual').onmouseup = depresIgual;
+
+// presion de teclado..
+document.onkeypress=presionaTecla;
